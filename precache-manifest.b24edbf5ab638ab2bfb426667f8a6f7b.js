fda97961e359a529f48fce17667448d9708c450f
darn0C <conradremon7@gmail.com>
2020-07-09 19:08:21 -0300

    Updates

---
+++ precache-manifest.b24edbf5ab638ab2bfb426667f8a6f7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f8c486dba8b3aba6d521bfe9b99df1",
+    "url": "/parallel-twitch/index.html"
+  },
+  {
+    "revision": "9bc67d880cab6dae38e7",
+    "url": "/parallel-twitch/static/js/2.5e179a47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/parallel-twitch/static/js/2.5e179a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53ccaab33c7f11477bf",
+    "url": "/parallel-twitch/static/js/main.6b36ebae.chunk.js"
+  },
+  {
+    "revision": "464ff191ea0fc41dfd56",
+    "url": "/parallel-twitch/static/js/runtime-main.34940a1d.js"
+  }
+]);
