0c19aa8b9c459422e2c732a2519715c9d399e46a
darn0C <conradremon7@gmail.com>
2020-07-21 21:32:27 -0300

    Add chat render and change function for all streams

--- src/components/StreamPage.jsx
+++ src/components/StreamPage.jsx
@@ -22,11 +22,19 @@ function StreamPage(props) {
             width: "100vw",
             backgroundColor: "#262626",
         },
+        streamSection: {
+            height: "100vh",
+            width: "85vw",
+        },
         streamContainer: {
             height: "50vh",
             width: "42.5vw",
             display: "inline-block",
             padding: "0"
+        },
+        chatSection: {
+            height: "100vh",
+            width: "15vw",
         },
         chatContainer: {
             height: "100vh",
@@ -38,6 +46,9 @@ function StreamPage(props) {
             backgroundColor: "#6441a5",
             color: "#b9a3e3",
             height: "5vh",
+            fontWeight: "900",
+            fontSize: "11px",
+            overflow: "hidden",
             "&:hover": {
                 backgroundColor: "#b9a3e3",
                 color: "#6441a5",
@@ -59,6 +70,14 @@ function StreamPage(props) {
 
     function handleSecondButton() {
         setCurrentChatInput(props.secondStream)
+    }
+
+    function handleThirdButton() {
+        setCurrentChatInput(props.thirdStream)
+    }
+
+    function handleFourthButton() {
+        setCurrentChatInput(props.fourthStream)
     }
 
     // RENDER STREAMS FUNCTION
@@ -119,60 +138,86 @@ function StreamPage(props) {
                             allowFullScreen="true">
                         </iframe>
                     </div>
-                    <div className={classes.chatContainer}>
-                        <Button className={classes.chatButton} onClick={handleFirstButton}
-                                style={{width: "50%"}}>{props.firstStream}</Button>
-                        <Button className={classes.chatButton} onClick={handleSecondButton}
-                                style={{width: "50%"}}>{props.secondStream}</Button>
-                        <iframe
-                            title="Chat window"
-                            frameBorder="0"
-                            scrolling="no"
-                            id="chat_embed"
-                            src={"https://www.twitch.tv/embed/" + currentChatInput + "/chat?parent=darn0c.github.io"}
-                            height="95%"
-                            width="100%">
-                        </iframe>
-                    </div>
+                    <Grid container direction="column" justify="center" alignItems="center"
+                          className={classes.chatSection}>
+                        <div className={classes.chatContainer}>
+                            <Button className={classes.chatButton} onClick={handleFirstButton}
+                                    style={{width: "50%"}}>{props.firstStream}</Button>
+                            <Button className={classes.chatButton} onClick={handleSecondButton}
+                                    style={{width: "50%"}}>{props.secondStream}</Button>
+                            <iframe
+                                title="Chat window"
+                                frameBorder="0"
+                                scrolling="no"
+                                id="chat_embed"
+                                src={"https://www.twitch.tv/embed/" + currentChatInput + "/chat?parent=darn0c.github.io"}
+                                height="95%"
+                                width="100%">
+                            </iframe>
+                        </div>
+                    </Grid>
                 </Grid>
             )
         } else if (props.howManyStreams === 3) {
             return (
                 <Grid container direction="row" justify="center" alignItems="center" className={classes.root}>
                     <CssBaseline/>
-                    <div className={classes.streamContainer}>
-                        <iframe
-                            title="First stream"
-                            src={"https://player.twitch.tv/?channel=" + props.firstStream + "&parent=darn0c.github.io"}
-                            height="100%"
-                            width="100%"
-                            frameBorder="0"
-                            scrolling="no"
-                            allowFullScreen="true">
-                        </iframe>
-                    </div>
-                    <div className={classes.streamContainer}>
-                        <iframe
-                            title="Second stream"
-                            src={"https://player.twitch.tv/?channel=" + props.secondStream + "&parent=darn0c.github.io"}
-                            height="100%"
-                            width="100%"
-                            frameBorder="0"
-                            scrolling="no"
-                            allowFullScreen="true">
-                        </iframe>
-                    </div>
-                    <div className={classes.streamContainer}>
-                        <iframe
-                            title="Third stream"
-                            src={"https://player.twitch.tv/?channel=" + props.thirdStream + "&parent=darn0c.github.io"}
-                            height="100%"
-                            width="100%"
-                            frameBorder="0"
-                            scrolling="no"
-                            allowFullScreen="true">
-                        </iframe>
-                    </div>
+                    <Grid container direction="row" justify="center" alignItems="center"
+                          className={classes.streamSection}>
+                        <div className={classes.streamContainer}>
+                            <iframe
+                                title="First stream"
+                                src={"https://player.twitch.tv/?channel=" + props.firstStream + "&parent=darn0c.github.io"}
+                                height="100%"
+                                width="100%"
+                                frameBorder="0"
+                                scrolling="no"
+                                allowFullScreen="true">
+                            </iframe>
+                        </div>
+                        <div className={classes.streamContainer}>
+                            <iframe
+                                title="Second stream"
+                                src={"https://player.twitch.tv/?channel=" + props.secondStream + "&parent=darn0c.github.io"}
+                                height="100%"
+                                width="100%"
+                                frameBorder="0"
+                                scrolling="no"
+                                allowFullScreen="true">
+                            </iframe>
+                        </div>
+                        <div className={classes.streamContainer}>
+                            <iframe
+                                title="Third stream"
+                                src={"https://player.twitch.tv/?channel=" + props.thirdStream + "&parent=darn0c.github.io"}
+                                height="100%"
+                                width="100%"
+                                frameBorder="0"
+                                scrolling="no"
+                                allowFullScreen="true">
+                            </iframe>
+                        </div>
+                    </Grid>
+                    <Grid container direction="column" justify="center" alignItems="center"
+                          className={classes.chatSection}>
+                        <div className={classes.chatContainer}>
+                            <Button className={classes.chatButton} onClick={handleFirstButton}
+                                    style={{width: "50%"}}>{props.firstStream}</Button>
+                            <Button className={classes.chatButton} onClick={handleSecondButton}
+                                    style={{width: "50%"}}>{props.secondStream}</Button>
+                            <Button className={classes.chatButton} onClick={handleThirdButton}
+                                    style={{width: "100%"}}>{props.thirdStream}</Button>
+                            <iframe
+                                title="Chat window"
+                                frameBorder="0"
+                                scrolling="no"
+                                id="chat_embed"
+                                src={"https://www.twitch.tv/embed/" + currentChatInput + "/chat?parent=darn0c.github.io"}
+                                height="90%"
+                                width="100%">
+                            </iframe>
+                        </div>
+                    </Grid>
                 </Grid>
 
             )
@@ -180,50 +225,75 @@ function StreamPage(props) {
             return (
                 <Grid container direction="row" justify="center" alignItems="center" className={classes.root}>
                     <CssBaseline/>
-                    <div className={classes.streamContainer}>
-                        <iframe
-                            title="First stream"
-                            src={"https://player.twitch.tv/?channel=" + props.firstStream + "&parent=darn0c.github.io"}
-                            height="100%"
-                            width="100%"
-                            frameBorder="0"
-                            scrolling="no"
-                            allowFullScreen="true">
-                        </iframe>
-                    </div>
-                    <div className={classes.streamContainer}>
-                        <iframe
-                            title="Second stream"
-                            src={"https://player.twitch.tv/?channel=" + props.secondStream + "&parent=darn0c.github.io"}
-                            height="100%"
-                            width="100%"
-                            frameBorder="0"
-                            scrolling="no"
-                            allowFullScreen="true">
-                        </iframe>
-                    </div>
-                    <div className={classes.streamContainer}>
-                        <iframe
-                            title="Third stream"
-                            src={"https://player.twitch.tv/?channel=" + props.thirdStream + "&parent=darn0c.github.io"}
-                            height="100%"
-                            width="100%"
-                            frameBorder="0"
-                            scrolling="no"
-                            allowFullScreen="true">
-                        </iframe>
-                    </div>
-                    <div className={classes.streamContainer}>
-                        <iframe
-                            title="Fourth stream"
-                            src={"https://player.twitch.tv/?channel=" + props.fourthStream + "&parent=darn0c.github.io"}
-                            height="100%"
-                            width="100%"
-                            frameBorder="0"
-                            scrolling="no"
-                            allowFullScreen="true">
-                        </iframe>
-                    </div>
+                    <Grid container direction="row" justify="center" alignItems="center"
+                          className={classes.streamSection}>
+                        <div className={classes.streamContainer}>
+                            <iframe
+                                title="First stream"
+                                src={"https://player.twitch.tv/?channel=" + props.firstStream + "&parent=darn0c.github.io"}
+                                height="100%"
+                                width="100%"
+                                frameBorder="0"
+                                scrolling="no"
+                                allowFullScreen="true">
+                            </iframe>
+                        </div>
+                        <div className={classes.streamContainer}>
+                            <iframe
+                                title="Second stream"
+                                src={"https://player.twitch.tv/?channel=" + props.secondStream + "&parent=darn0c.github.io"}
+                                height="100%"
+                                width="100%"
+                                frameBorder="0"
+                                scrolling="no"
+                                allowFullScreen="true">
+                            </iframe>
+                        </div>
+                        <div className={classes.streamContainer}>
+                            <iframe
+                                title="Third stream"
+                                src={"https://player.twitch.tv/?channel=" + props.thirdStream + "&parent=darn0c.github.io"}
+                                height="100%"
+                                width="100%"
+                                frameBorder="0"
+                                scrolling="no"
+                                allowFullScreen="true">
+                            </iframe>
+                        </div>
+                        <div className={classes.streamContainer}>
+                            <iframe
+                                title="Fourth stream"
+                                src={"https://player.twitch.tv/?channel=" + props.fourthStream + "&parent=darn0c.github.io"}
+                                height="100%"
+                                width="100%"
+                                frameBorder="0"
+                                scrolling="no"
+                                allowFullScreen="true">
+                            </iframe>
+                        </div>
+                    </Grid>
+                    <Grid container direction="column" justify="center" alignItems="center"
+                          className={classes.chatSection}>
+                        <div className={classes.chatContainer}>
+                            <Button className={classes.chatButton} onClick={handleFirstButton}
+                                    style={{width: "50%"}}>{props.firstStream}</Button>
+                            <Button className={classes.chatButton} onClick={handleSecondButton}
+                                    style={{width: "50%"}}>{props.secondStream}</Button>
+                            <Button className={classes.chatButton} onClick={handleThirdButton}
+                                    style={{width: "50%"}}>{props.thirdStream}</Button>
+                            <Button className={classes.chatButton} onClick={handleFourthButton}
+                                    style={{width: "50%"}}>{props.fourthStream}</Button>
+                            <iframe
+                                title="Chat window"
+                                frameBorder="0"
+                                scrolling="no"
+                                id="chat_embed"
+                                src={"https://www.twitch.tv/embed/" + currentChatInput + "/chat?parent=darn0c.github.io"}
+                                height="90%"
+                                width="100%">
+                            </iframe>
+                        </div>
+                    </Grid>
                 </Grid>
             )
         }
